8f624647540d3d64e0bb5fd3d5f717c311654a67
MankovskiPavel <mankovskipavel@gmail.com>
2021-02-28 19:19:19 +0300

    fix solution: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,7 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
 if (typeof sampleActivity!="string") {return false}
-if ((activity>0)&&(activity<MODERN_ACTIVITY)) {return Math.ceil(Math.log(MODERN_ACTIVITY/activity)*HALF_LIFE_PERIOD/0.693)}
+ let activity=Number(sampleActivity)
+ if ((activity>0)&&(activity<MODERN_ACTIVITY)) {return Math.ceil(Math.log(MODERN_ACTIVITY/activity)*HALF_LIFE_PERIOD/0.693)}
     else {return false}
 }
