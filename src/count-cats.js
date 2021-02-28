10120d2cd38a1500f36fad9370a425a4011f288f
MankovskiPavel <mankovskipavel@gmail.com>
2021-02-28 18:23:59 +0300

    solution.couny-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function countCats(array) {
+  let i=0
+  for(let i2=0;i2<array.length;i2++){
+    for(let i3=0;i3<array[i2].length;i3++){
+      if (array[i2][i3]=='^^'){i++}
+    }
+  }
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+ return i
 };
