a340bba80c867ac7eaa34775e3476cc3485b6607
MankovskiPavel <mankovskipavel@gmail.com>
2021-03-06 15:57:08 +0300

    mysolution_extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let newst=str
+
+  if (options["additionRepeatTimes"]==undefined){options.additionRepeatTimes=0}
+  if (options["RepeatTimes"]==undefined){options.RepeatTimes=0}
+
+  if(options["separator"]==undefined){   
+    options.separator="+"
+    }
+  let add=""
+
+
+if(options["addition"]!==undefined){
+    if(options["additionSeparator"]==undefined){options.additionSeparator="|"}
+  
+  add+=options["addition"] 
+ // console.log("add+aditonal="+add)
+  for(let j=1;j<options['additionRepeatTimes'];j++){
+    add+=options["additionSeparator"]+options["addition"]
+  }
+}  
+newst+=add
+let na=[]
+na.push(str+add)
+
+  for(let i=1;i<options['repeatTimes'];i++){
+    na.push(str+add)       
+    }
+  return na.join(options.separator)
 };
   
