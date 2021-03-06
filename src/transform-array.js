3c0c385836ae3e8eab5d4ff656be39ca1137ab31
MankovskiPavel <mankovskipavel@gmail.com>
2021-03-06 18:33:49 +0300

    try1

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let narr=[]
+ // console.log(arr)
+  if(!Array.isArray(arr)){throw new Error();}
+  arr.map(function(element,index){
+    console.log("elemnt="+element)
+    console.log("index="+index)
+    if (element=='--double-next'){if(element[index+1]!=undefined){narr.push()};return}
+    if (element=='--discard-prev'){if(narr.length!=0){narr.pop()};return}
+    if (element=='--double-prev'){if(narr.length!=0){narr.push(element[index-1])};return}
+    if (element[index-1]=='--discard-next'){return}
+    narr.push(element)
+  })
+  return narr
 };
