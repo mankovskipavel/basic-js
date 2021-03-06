f37b581b28cccc54a1331a330c3607989abc104c
MankovskiPavel <mankovskipavel@gmail.com>
2021-03-06 19:48:23 +0300

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-
+  if(!date){return 'Unable to determine the time of year!'}
  if(Object.prototype.toString.call(date)!=="[object Date]"){throw new Error()};
   let month=date.getMonth()+1
   //console.log("date="+date)
